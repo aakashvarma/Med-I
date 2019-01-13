e00dc6f601e6f0dee5594f3113b2fc2dcc2e2acb
aakashvarma <aakashvarma18@gmail.com>
2019-01-13 22:40:34 +0530

    html css added

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,10 @@
 let express = require('express');
 let mongoose = require('mongoose');
-let imageRoutes = require('./routes/image');
 let bodyParser = require('body-parser');
-var path = require('path');
+let path = require('path');
+let imageRoutes = require('./routes/image');
+let mainRoutes = require('./routes/main')
+
 
 
 // setup express app
@@ -14,12 +16,13 @@ mongoose.Promise = global.Promise;
 
 // use the body parser
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/public', express.static('public'))
 app.set('view engine', 'ejs');
 
 // redirect to the api routes
+app.use('', mainRoutes);
 app.use('/image', imageRoutes);
-
+ 
 app.use(function(err, req, res, next){
     res.status(422);
     res.send({error: err.message});
