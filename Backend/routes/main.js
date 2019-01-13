4ffa8765aeac4c7d76e178d1f18b39fd5eaf48c9
aakashvarma <aakashvarma18@gmail.com>
2019-01-13 23:56:00 +0530

    upload route added

--- Backend/routes/main.js
+++ Backend/routes/main.js
@@ -6,6 +6,10 @@ router.get('/', (req, res) => {
     res.render('index');
 });
 
+router.get('/upload', (req, res) => {
+    res.send("this is the uploads page.");
+});
+
 module.exports = router;
 
 
